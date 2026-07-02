4999771c351e3590cdc3ff48b85d705036422646
Shrutikathal <shrutikathal3023@gmail.com>
2026-07-02 11:43:27 +0530

    Auto‑commit 2026-07-02T06:13:27.363Z

---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function OpenAccount() {
+    return (
+        <h1>Open an Account</h1>            
+    );
+}
+
+export default OpenAccount;
--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Hero from './Hero';
+import Awards from './Awards';
+import Stats from './Stats';
+import Pricing from './Pricing';
+import Education from './Education';
+import OpenAccount from '../OpenAccount';
+import Footer from '../Footer';
+import Navbar from '../Navbar';
 
 function HomePage() {
     return ( 
-        <h1>HomePage</h1>
+        // React Fragments (<> </>) — Most Likely
+        // In React development, developers frequently refer to the empty tag syntax as an "empty flag" or "empty tag.
+        // In React, a component can only return a single parent element. 
+        // If you want to return multiple HTML elements next to each other, you used to have to wrap them in a <div>. 
+        // This created unnecessary clutter in the HTML structure (known as "div soup").
+        // To fix this, React introduced Fragments, which look like empty HTML tags. 
+        // They group elements together without adding extra nodes to the actual DOM.
+
+        <>
+            <Navbar />
+            <Hero />
+            <Awards />
+            <Stats />
+            <Pricing />
+            <Education />
+            <OpenAccount />
+            <Footer />
+            
+
+
+        </>
      );
 }
 
